34c4e57c81926ef6e48ca353bed549ed06c97337
Admin <art932008@gmail.com>
2022-06-23 14:27:12 +0300

    practice2

--- src/org/itmo/java/practice2/Main.java
+++ src/org/itmo/java/practice2/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Person me = new Person("Artjom", "Kozlov", "Ingineer", 28, false);
         System.out.println(me);
+        System.out.println();
         Person notMe = new Person("Somepers", "Somepersonov", "Some specialist", 30, true);
         System.out.println(notMe);
     }
